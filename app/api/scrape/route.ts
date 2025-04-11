25c4ce125898a9477f1fb8eefeee866c1683f92a
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2025-04-11 17:13:36 +0300

    FIX | changed chromium import

--- app/api/scrape/route.ts
+++ app/api/scrape/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import puppeteer from "puppeteer";
+import chromium from "@sparticuz/chromium";
 
 export const dynamic = "force-dynamic"; // Ensure it runs server-side, not edge
 
@@ -12,8 +13,10 @@ export async function POST(req: NextRequest) {
 
   try {
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
     });
 
     const page = await browser.newPage();
